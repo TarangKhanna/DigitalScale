11f61dd50553ca2e26e546d6b72a997ca9a3a8ce
Tarang Khanna <khanna17@purdue.edu>
2015-12-28 16:18:58 +0530

    can weigh now
    uses force from sensor and converts to grams

--- DigitalScale/ViewController.swift
+++ DigitalScale/ViewController.swift
@@ -9,17 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var weightLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if let touch = touches.first {
+            if traitCollection.forceTouchCapability == UIForceTouchCapability.Available {
+                if touch.force >= touch.maximumPossibleForce {
+                    weightLabel.text = "385+ grams"
+                } else {
+                    let force = touch.force/touch.maximumPossibleForce
+                    let grams = force * 385
+                    weightLabel.text = "\(Int(grams)) grams"
+                }
+            }
+        }
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        weightLabel.text = "0 grams"
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
